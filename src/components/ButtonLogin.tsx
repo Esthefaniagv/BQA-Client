c4bde0a87990227ca998204c6db1c568958759a1
esthefaniagv <esthefaniagv@gmail.com>
2023-09-05 18:18:51 -0300

    modificacion de componentes input y button, prueba de API

--- src/components/ButtonLogin.tsx
+++ src/components/ButtonLogin.tsx
@@ -1,18 +1,11 @@
 import { Fragment } from 'react';
 
 export const ButtonLogin = () => {
+  
+
   return (
     <Fragment>
-<div className="input-group mb-3 ml-5" id="inputGroup">
-  
-      <input type="checkbox" aria-label="Checkbox for following text input"/>
 
-  <p className='rememberCheck'>Remember me</p>
-</div>
-
-<div>
-<button className="btn btn-lg" type="submit" value="Submit" id='buttonLogin'>Iniciar Sesión</button>
-</div>
 
     </Fragment>
   )
--- src/components/LoginInput.tsx
+++ src/components/LoginInput.tsx
@@ -1,17 +1,45 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 export const LoginInput = () => {
+  const [message, setMessage] = useState('');
+
+  const handleChange = event => {
+    setMessage(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  const logMovies = async () => {
+    const options = {
+      method: "POST"
+    };
+    const response = await fetch("http://localhost:8080/login", options);
+    const movies = await response.json();
+    console.log(movies);
+  }
+
   return (
     <Fragment>
-      <div className='form-group'>
-        <div className='col-xs-1 mb-3'>
-          <input className='form-control p-3 m-0' id="formInputUser" placeholder='Email' type='text' />
+      <form className='form-group inputForm'>
+        <div className='col-xs-1'>
+          <input className='form-control p-3 m-0' id="formInputUser" placeholder='Email' type='text' value={message} onChange={handleChange} />
         </div>
 
         <div className='col-xs-1'>
           <input className='form-control p-3 m-0' id="formInputPassword" placeholder='Contraseña' type='password' />
         </div>
-      </div>
+
+        <div className="input-group ml-5" id="inputGroup">
+
+          <input type="checkbox" aria-label="Checkbox for following text input" />
+
+          <p className='rememberCheck'>Remember me</p>
+        </div>
+
+        <div>
+          <button className="btn btn-lg" type="submit" value="Submit" id='buttonLogin'>Iniciar Sesión</button>
+        </div>
+      </form>
     </Fragment>
   );
 };
