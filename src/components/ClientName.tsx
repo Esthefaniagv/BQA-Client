67341730767136c7b455303f0498662ea24b706b
esthefaniagv <esthefaniagv@gmail.com>
2023-09-15 11:31:47 -0300

    prueba sobre input para obtener nombre cliente

--- src/components/ClientName.tsx
+++ src/components/ClientName.tsx
@@ -1,10 +1,17 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
 
 export const ClientName = () => {
+    const [clientName, setClientName] = useState('');
+
+    const handleClientName = (e) =>{
+        setClientName(e.target.value)
+    }
+
     return (
         <Fragment>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Escribe nombre de cliente aquí" aria-label="Username" aria-describedby="basic-addon1"/>
+                <input onChange={handleClientName} value={clientName} type="text" className="form-control" placeholder="Escribe nombre de cliente aquí" aria-label="Username" aria-describedby="basic-addon1"/>
             </div>
         </Fragment>
     )
--- src/components/ClientOrder.tsx
+++ src/components/ClientOrder.tsx
@@ -1,10 +1,11 @@
 import { Fragment } from "react"
 import { ItemsBoxResume } from "./ItemsBoxResume"
 
-export const ClientOrder = () => {
+export const ClientOrder = ({clientName}) => {
   return (
     <Fragment>
     <div className="clientOrder">ClientOrder
+    <p>{clientName}</p>
     <ItemsBoxResume/>
     </div>
     </Fragment>
