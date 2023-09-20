607a86df67257968f89d40ffa3203ca99bb0465e
esthefaniagv <esthefaniagv@gmail.com>
2023-09-20 10:26:54 -0300

    pruebas de form para guardar nombre de cliente

--- src/components/ItemsBoxResume.tsx
+++ src/components/ItemsBoxResume.tsx
@@ -4,127 +4,130 @@ import { IProduct } from './ItemsBox';
 export const ItemsBoxResume = ({ selectedProduct }: { selectedProduct: IProduct }) => {
 
   const [clientName, setClientName] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  const SubmitProducts = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const payLoad = Object.fromEntries(formData);
+
+    console.log(payLoad)
+  }
 
   const handleClientName = (e) => {
     setClientName(e.target.value)
   }
-  const [quantity, setQuantity] = useState(1)
-
-  
   
 
-  const sumQuantity= () => {
-      setQuantity( quantity + 1)
-  }
-  
-  function quantities({ numberOne, sumQuantity }) {
-    return <p>{numberOne}</p>;
-  }
-//   const resQuantity= () => {
-//     setQuantity(quantity - 1)
-// }
-
-// const sumar = useMemo(() => {
-//   setQuantity(quantity + 1)
-// },[quantity])
-
-  // const increaseQuantity = (selectedProduct) => {
-  //   return (event: React.MouseEvent) => {
-  //     setQuantity(selectedProduct.qty + 1);
-  //     event.preventDefault();
-  //   }
+  // const sumQuantity = () => {
+  //   setQuantity(quantity + 1)
   // }
 
-  // const decreaseQuantity = (selectedProduct) => {
-  //   return (event: React.MouseEvent) => {
-  //     if (quantity > 0) {
-  //       setQuantity(selectedProduct.qty - 1);
-  //     }
-  //     event.preventDefault();
-  //   }
+  // function quantities({ numberOne, sumQuantity }) {
+  //   return <p>{numberOne}</p>;
   // }
+  // const resQuantity = () => {
+  //   setQuantity(quantity - 1)
+  // }
+
+  // const sumar = useMemo(() => {
+  //   setQuantity(quantity + 1)
+  // },[quantity])
+
+  const increaseQuantity = (selectedProduct) => {
+      setQuantity(selectedProduct.qty + 1);
+
+  }
+
+  const decreaseQuantity = (selectedProduct) => {
+      if (quantity > 0) {
+        setQuantity(selectedProduct.qty - 1);
+      }
+    
+  }
 
 
   return (
     <>
       <div className='sectionItemsResume'>
-        <section>
+        <form onSubmit={SubmitProducts}>
           <div className="clientName">
-              <input value={clientName} onChange={handleClientName} type="text" className="form-control mb-3" placeholder="Escribe nombre de cliente aquí" aria-label="Username" aria-describedby="basic-addon1" />
-              <p>Cuenta de {clientName}</p>
+            <input value={clientName} onChange={handleClientName} type="text" name='clientName' className="form-control mb-3" placeholder="Escribe nombre de cliente aquí" aria-label="Username" aria-describedby="basic-addon1" />
           </div>
-          
-          <div className='divItemsResume'>
-            {selectedProduct.map((selectedProduct) => (
-              <>
+          <section>
+            <div className='divItemsResume'>
+              {selectedProduct.map((selectedProduct) => (
+                <>
 
-                <div className='productImgResume'>
-                  <img src={selectedProduct.image} alt='imagen Producto' />
+                  <div className='productImgResume'>
+                    <img src={selectedProduct.image} alt='imagen Producto' />
 
-                  <p className='nameProductResume'>{selectedProduct.name}</p>
+                    <p className='nameProductResume'>{selectedProduct.name}</p>
 
-                  <p className='priceProduct'>${selectedProduct.price}</p>
-                </div>
-
-                <div className='divIconsResume'>
-                  <div className='iconsPlusLess'>
-                    <button className="buttonDecrease" onClick={resQuantity}>
-                      <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='20'
-                        height='20'
-                        fill='currentColor'
-                        className='bi bi-dash-circle-fill'
-                        viewBox='0 0 16 16'
-                      >
-                        <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z' />
-                      </svg>
-                    </button>
-                    <p id="productQuantity">{quantity}</p>
-                    <button className="buttonIncrease" onClick={sumQuantity}>
-                      <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='20'
-                        height='20'
-                        fill='currentColor'
-                        className='bi bi-plus-circle-fill'
-                        viewBox='0 0 16 16'
-                      >
-                        <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z' />
-                      </svg>
-                    </button>
-
+                    <p className='priceProduct'>${selectedProduct.price}</p>
                   </div>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='20'
-                    height='20'
-                    fill='currentColor'
-                    className='bi bi-trash3-fill'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z' />
-                  </svg>
-                </div>
 
-              </>
-            ))}
+                  <div className='divIconsResume'>
+                    <div className='iconsPlusLess'>
+                      <button className="buttonDecrease" onClick={decreaseQuantity}>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          width='20'
+                          height='20'
+                          fill='currentColor'
+                          className='bi bi-dash-circle-fill'
+                          viewBox='0 0 16 16'
+                        >
+                          <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z' />
+                        </svg>
+                      </button>
+                      <p id="productQuantity">{selectedProduct.qty}</p>
+                      <button className="buttonIncrease" onClick={increaseQuantity}>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          width='20'
+                          height='20'
+                          fill='currentColor'
+                          className='bi bi-plus-circle-fill'
+                          viewBox='0 0 16 16'
+                        >
+                          <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z' />
+                        </svg>
+                      </button>
+
+                    </div>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      width='20'
+                      height='20'
+                      fill='currentColor'
+                      className='bi bi-trash3-fill'
+                      viewBox='0 0 16 16'
+                    >
+                      <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z' />
+                    </svg>
+                  </div>
+
+                </>
+              ))}
+            </div>
+          </section>
+          <p className='dots'>-------------------------------------</p>
+          <div className='divParagraph'>
+            <p className='totaltext'>TOTAL</p>
+            <p className='totalResult'>0</p>
           </div>
-        </section>
-        <p className='dots'>-------------------------------------</p>
-        <div className='divParagraph'>
-          <p className='totaltext'>TOTAL</p>
-          <p className='totalResult'>0</p>
-        </div>
-        <button
-          data-testid='buttonLogin'
-          className='btn btn-lg'
-          type='submit'
-          value='Submit'
-          id='buttonOrderChefView'
-        >
-          ENVIAR A COCINA
-        </button>
+          <button
+            data-testid='buttonLogin'
+            className='btn btn-lg'
+            type='submit'
+            value='Submit'
+            id='buttonOrderChefView'
+          >
+            ENVIAR A COCINA
+          </button>
+        </form>
       </div>
     </>
   );
--- src/services/TokenProducts.ts
+++ src/services/TokenProducts.ts
@@ -3,7 +3,7 @@ export const GetProducts = () => {
         method: 'GET',
         headers: {
             'content-type': 'application/json',
-            'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5NTA4NzI2OCwiZXhwIjoxNjk1MDkwODY4LCJzdWIiOiIyIn0.SZCvuZrhn0VVtwXoa2m8uNMqlLDooL0v97zJt9jpQ7M'
+            'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5NTIxNDY2OSwiZXhwIjoxNjk1MjE4MjY5LCJzdWIiOiIyIn0.YSrHuxjHUXZF-p0mVJGu3d2z-sH2WMIFinVR01qoAAQ'
         },
     };
     return fetch('http://localhost:8080/products', options);
